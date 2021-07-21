9e6ea0346712433e9611062537cd17680d3b803a
Matheus Alencar <mateus.alencar@f5promotora.com.br>
2021-07-21 09:17:50 -0300

    feat:add novos parametros

--- main.py
+++ main.py
@@ -8,16 +8,17 @@ from geneticalgorithm import geneticalgorithm as ga
 import random as rd
 
 dados = pd.read_csv('./data.csv')
-velocidade = np.asarray(dados.iloc[:,0])
-temperatura = np.asarray(dados.iloc[:,1])
-preenchimento = np.asarray(dados.iloc[:,2])
-espessura = np.asarray(dados.iloc[:,3])
-orientacao = np.asarray(dados.iloc[:,4])
-resistencia = np.asarray(dados.iloc[:,5])
+velocidade = np.asarray(dados.iloc[:, 0])
+temperatura = np.asarray(dados.iloc[:, 1])
+preenchimento = np.asarray(dados.iloc[:, 2])
+espessura = np.asarray(dados.iloc[:, 3])
+orientacao = np.asarray(dados.iloc[:, 4])
+resistencia = np.asarray(dados.iloc[:, 5])
 
 x = np.c_[velocidade, temperatura, preenchimento, espessura, orientacao]
 y = resistencia
 x_treino, x_teste, y_treino, y_teste = train_test_split(x, y)
+
 
 def conversorBinarioReal(binario):
     v = 0
@@ -25,16 +26,33 @@ def conversorBinarioReal(binario):
         v += binario[i] * (2 ** (- i - 1))
     return v
 
+
 def aptidao(x):
-    learning_rate_init=conversorBinarioReal(x[0:29])
-    regr = MLPRegressor(random_state=1, max_iter=10000, learning_rate_init=learning_rate_init).fit(x_treino, y_treino)
+    learning_rate_init = conversorBinarioReal(x[:29])
+    momentum = conversorBinarioReal(x[30:])
+    regr = MLPRegressor(random_state=1,
+                        max_iter=20000,
+                        learning_rate_init=learning_rate_init,
+                        solver='sgd',
+                        momentum=momentum).fit(x_treino, y_treino)
     # pred = regr.predict(x_teste)
     score = regr.score(x_teste, y_teste)
     return -score
 
-pop_i = np.array([[0,1]]*50)
 
-model = ga(function=aptidao, dimension=50, variable_type='int', variable_boundaries=pop_i)
+algorithm_param = {'max_num_iteration': 3000,
+                   'population_size': 100,
+                   'mutation_probability': 0.1,
+                   'elit_ratio': 0.01,
+                   'crossover_probability': 0.5,
+                   'parents_portion': 0.3,
+                   'crossover_type': 'uniform',
+                   'max_iteration_without_improv': None}
+
+pop_i = np.array([[0, 1]]*60)
+
+model = ga(function=aptidao, dimension=60, function_timeout=60,
+           variable_type='int', variable_boundaries=pop_i, algorithm_parameters=algorithm_param)
 model.run()
 # print(score)
 # x_linha = np.linspace(0, 60, 1000)
