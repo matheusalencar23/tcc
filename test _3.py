03a74b916b022b8592e9fb6588e4f5a252c296f4
Matheus Alencar <matheusalencar6942@gmail.com>
2021-09-07 18:45:13 -0300

    ajustes

--- test _3.py
+++ test _3.py
@@ -53,14 +53,16 @@ def regr(ind, x, y):
         conversorBinarioInteiro(ind[112:]))
     print(learning_rate_init, beta_1, beta_2, epsilon, hidden_layer_sizes)
     regr = MLPRegressor(random_state=1, learning_rate_init=learning_rate_init, shuffle=True,
-                        max_iter=100, beta_1=beta_1, beta_2=beta_2, epsilon=epsilon,
+                        max_iter=1000, beta_1=beta_1, beta_2=beta_2, epsilon=epsilon,
                         solver='adam', activation='relu', learning_rate='constant',
                         hidden_layer_sizes=hidden_layer_sizes).fit(x, y)
     return regr
 
 
-ind = np.asarray([0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 1, 1, 0, 1, 0,
-                 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 1, 1, 0, 1, 0, 1, 0, 0, 1])
+ind = np.asarray([0,0,0,0,0,0,0,1,1,1,0,0,0,1,1,0,0,0,0,0,0,1,0,0,1,1,1,0,0,1,1,0,1,1,1,0,1
+,1,0,0,0,1,0,0,0,0,0,0,0,0,1,1,1,1,1,0,0,0,0,1,0,1,1,0,1,0,0,1,0,1,0,0,0,0
+,0,0,0,0,0,0,0,0,1,0,0,0,1,1,0,0,1,1,1,0,1,0,0,1,0,1,0,1,1,1,0,1,0,0,1,1,0
+,1,1,1,1,0,0,0])
 regr_treino = regr(ind, x_treino, y_treino)
 regr_teste = regr(ind, x_teste, y_teste)
 pred_treino = regr_treino.predict(x_treino)
@@ -72,11 +74,6 @@ print('R^2 teste: {}'.format(regr_teste.score(x_teste, y_teste)))
 print('MSE teste: {}'.format(mean_squared_error(pred_teste, y_teste)))
 print('EVS teste: {}'.format(explained_variance_score(pred_teste, y_teste)))
 
-plt.subplot(1, 2, 1)
-plt.plot(regr_treino.loss_curve_, 'g')
+plt.plot(regr_treino.loss_curve_, 'b')
 plt.plot(regr_teste.loss_curve_, 'r')
-plt.subplot(1, 2, 2)
-plt.plot(np.linspace(0, 60, 100), np.linspace(0, 60, 100), 'b')
-plt.plot(y_treino, pred_treino, 'go')
-plt.plot(y_teste, pred_teste, 'ro')
 plt.show()
